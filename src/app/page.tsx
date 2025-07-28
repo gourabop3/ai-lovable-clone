441567eb4c0c774066ea1f36bb9a7fe8a69fdd84
gourabop3 <gourabmullick200@gmail.com>
2025-07-28 15:18:14 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
-import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { Button } from '@/components/ui/button';
+import { useTRPC } from '@/trpc/client';
+import { useMutation } from '@tanstack/react-query';
 
 export default function Home() {
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({}))
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+
   return (
     <div className="p-4 max-w-7xl mx-auto">
-      <Button onClick={()=> }>
+      <Button onClick={() => invoke.mutate()}>
         Invoke Background Job
       </Button>
     </div>
